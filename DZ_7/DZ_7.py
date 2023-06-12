2c1523eb60d8ff687bc6a7f49aab1b85b0cd7af7
1SergPav1 <pav220521@gmail.com>
2023-06-12 11:09:22 +0300

    DZ_7

--- DZ_7/DZ_7.py
+++ DZ_7/DZ_7.py
@@ -1,3 +1,29 @@
+# Задача 34:  Винни-Пух попросил Вас посмотреть, есть ли в его стихах ритм. Поскольку разобраться в его кричалках не
+# настолько просто, насколько легко он их придумывает, Вам стоит написать программу. Винни-Пух считает, что ритм есть,
+# если число слогов (т.е. число гласных букв) в каждой фразе стихотворения одинаковое. Фраза может состоять из одного
+# слова, если во фразе несколько слов, то они разделяются дефисами. Фразы отделяются друг от друга пробелами.
+# Стихотворение  Винни-Пух вбивает в программу с клавиатуры. В ответе напишите “Парам пам-пам”, если с ритмом все в
+# порядке и “Пам парам”, если с ритмом все не в порядке.
+# **Ввод:** пара-ра-рам рам-пам-папам па-ра-па-да
+# **Вывод:** Парам пам-пам
+
+target_list = ['а', 'е', 'ё', 'и', 'о', 'у', 'ы', 'э', 'ю', 'я']
+result = []
+
+poem = input('Cтих: ').split(' ')
+
+for phrase in poem:
+    count = 0
+    for letter in phrase:
+        if letter in target_list:
+            count += 1
+    result.append(count)
+
+if all(i == result[0] for i in result):
+    print('Парам пам-пам')
+else:
+    print('Пам парам')
+
 # Задача 36: Напишите функцию print_operation_table(operation, num_rows=6, num_columns=6), которая принимает в качестве
 # аргумента функцию, вычисляющую элемент по номеру строки и столбца. Аргументы num_rows и num_columns указывают число
 # строк и столбцов таблицы, которые должны быть распечатаны. Нумерация строк и столбцов идет с единицы (подумайте,
@@ -12,14 +38,14 @@
 # 5 10 15 20 25 30
 # 6 12 18 24 30 36
 
-def print_operation_table(operation, num_rows, num_columns):
-    result_table = []
-    rows = [i+1 for i in range(num_rows)]
-    colums = [i+1 for i in range(num_columns)]
-    for i in rows:
-        result_table.append([operation(i, j) for j in colums])
-    for i in result_table:
-        print(i)
-
-
-print_operation_table(lambda a, b: a * b, 6, 6)
+# def print_operation_table(operation, num_rows, num_columns):
+#     result_table = []
+#     rows = [i+1 for i in range(num_rows)]
+#     colums = [i+1 for i in range(num_columns)]
+#     for i in rows:
+#         result_table.append([operation(i, j) for j in colums])
+#     for i in result_table:
+#         print(i)
+#
+#
+# print_operation_table(lambda a, b: a * b, 6, 6)
